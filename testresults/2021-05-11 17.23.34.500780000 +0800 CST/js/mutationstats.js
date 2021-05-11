32d3d19c809bbaa8f68b07439598b96aee6308b8
Ping Zhu <pingzhu@Pings-MacBook-Pro.local>
2021-05-11 17:23:59 +0800

    m

--- testresults/2021-05-11 17.23.34.500780000 +0800 CST/js/mutationstats.js
+++
@@ -1,6 +0,0 @@
-
-var mutationStats1 = [{"ReportKey":{"Path":"https://api.douban.com/v2/movie/subject/1292052","Method":"GET","ActualStatusCode":403},"Count":11}]
-
-var mutationStats2 = [{"ReportKey":{"Path":"https://api.douban.com/v2/movie/subject/1292052","Method":"GET","MutationArea":"","ActualStatusCode":403},"Count":11}]
-
-var mutationStats3 = [{"ReportKey":{"Path":"https://api.douban.com/v2/movie/subject/1292052","Method":"GET","MutationArea":"","MutationCategory":"","ActualStatusCode":403},"Count":11}]
